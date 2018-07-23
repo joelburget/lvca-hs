ce9c8b046697399a6ca4940bb73f35a43343e55b
Joel Burget <joelburget@gmail.com>
2018-07-23 07:53:19 -0700

    Add judgements.

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,15 @@
+{-# LANGUAGE LambdaCase        #-}
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import           Data.List       (intersperse)
 import           Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
 import           Data.Text       (Text)
+import           Data.String     (IsString(fromString))
+
+import           Data.Text.Prettyprint.Doc
+import           Data.Text.Prettyprint.Doc.Render.Text
 
 newtype SyntaxChart = SyntaxChart (Map Text Sort)
 
@@ -14,46 +20,157 @@ data Sort = Sort
 
 data Operator = Operator
   !Text    -- ^ operator name
-  !Arity
+  !Arity   -- ^ arity
   !Text    -- ^ description
 
 data Arity = Arity
   ![Valence] -- ^ the valences
   !Text      -- ^ the resulting sort
 
+data InOut = In | Out
+
+data JudgementForm = JudgementForm
+  !Text            -- ^ name of the judgement
+  ![(InOut, Text)] -- ^ mode and sort of all slots
+
+data OperatorApplication = OperatorApplication
+  !Text            -- ^ operator name
+  ![SaturatedTerm] -- applicands
+
+data SaturatedTerm
+  = Variable Text
+  | Op OperatorApplication
+
+instance IsString SaturatedTerm where
+  fromString = Variable . fromString
+
+infix 2 @@
+(@@) :: Text -> [SaturatedTerm] -> SaturatedTerm
+(@@) a b = Op (OperatorApplication a b)
+
+data JudgementClause = JudgementClause
+  !Text            -- ^ head (name of judgement)
+  ![SaturatedTerm] -- ^ applicands
+
+infix 2 @@@
+(@@@) :: Text -> [SaturatedTerm] -> JudgementClause
+(@@@) = JudgementClause
+
+infix 2 %%%
+(%%%) :: [SaturatedTerm] -> Text -> JudgementClause
+(%%%) = flip JudgementClause
+
+data JudgementRule = JudgementRule
+  ![JudgementClause] -- ^ assumptions
+  !JudgementClause   -- ^ conclusion
+
+infix 0 .--
+(.--) :: [JudgementClause] -> JudgementClause -> JudgementRule
+(.--) = JudgementRule
+
+newtype JudgementRules = JudgementRules [JudgementRule]
+
+instance Pretty OperatorApplication where
+  pretty (OperatorApplication hd applicands)
+    = pretty hd <+> hsep (pretty <$> applicands)
+
+instance Pretty SaturatedTerm where
+  pretty = \case
+    Variable name -> pretty name
+    Op opAp       -> pretty opAp
+
+instance Pretty JudgementClause where
+  pretty (JudgementClause hd applicands)
+    = pretty hd <+> hsep (pretty <$> applicands)
+
+instance Pretty JudgementRule where
+  pretty (JudgementRule assumptions conclusion) = vsep
+    [ hsep (pretty <$> assumptions)
+    , "------"
+    , pretty conclusion
+    ]
+
+instance Pretty JudgementRules where
+  pretty (JudgementRules rules) = vsep (intersperse "" (pretty <$> rules))
+
+--
+
+natJudgement :: JudgementForm
+natJudgement = JudgementForm "nat" [(In, "a")]
+
+natJudgements :: JudgementRules
+natJudgements = JudgementRules
+  [ []
+    .--
+    ["zero"] %%% "nat"
+  , [["a"] %%% "nat"]
+    .--
+    ["succ" @@ ["a"]] %%% "nat"
+  ]
+
+--
+
+instance Pretty SyntaxChart where
+  pretty (SyntaxChart sorts) =
+    let f (title, operators) = vsep [pretty title <> ":", indent 2 $ pretty operators]
+    in vsep (f <$> Map.toList sorts)
+
+instance Pretty Sort where
+  pretty (Sort _vars operators)
+    = vsep (pretty <$> operators)
+
+instance Pretty Operator where
+  pretty (Operator name arity _desc)
+    = hsep [pretty name <> ":", pretty arity]
+
+instance Pretty Arity where
+  pretty (Arity valences result) =
+    let valences' = if null valences
+          then mempty
+          else parens (hsep (punctuate comma (pretty <$> valences)))
+    in valences' <> pretty result
+
 data Valence = Valence
   ![Text] -- ^ the sorts of all bound variables
   Text    -- ^ the resulting sort
+
+instance Pretty Valence where
+  pretty (Valence boundVars result) = hsep $
+    punctuate dot (fmap pretty boundVars <> [pretty result])
 
 eChart :: SyntaxChart
 eChart = SyntaxChart $ Map.fromList
   [ ("Typ", Sort ["t"]
     -- TODO: is this the correct arity (sort)?
-    [ Operator "num" (Arity [] "t") "numbers"
-    , Operator "str" (Arity [] "t") "strings"
+    [ Operator "num" (Arity [] "Typ") "numbers"
+    , Operator "str" (Arity [] "Typ") "strings"
     ])
   , ("Exp", Sort ["e"]
-    -- TODO: is this the correct name and arity?
-    [ Operator "var" (Arity [] "e") "variable"
+    [ Operator "var" (Arity [] "Exp") "variable"
     -- TODO:
     -- * do we have to really specify the resulting sort every time?
     -- * how do we refer to n, which is understood to be a number?
-    , Operator "num" (Arity [Valence [] "n"] "e") "numeral"
-    , Operator "str" (Arity [Valence [] "s"] "e") "literal"
-    -- TODO: how do we give a unique name to these bound "e"s?
-    , Operator "plus" (Arity [Valence [] "e", Valence [] "e"] "e") "addition"
-    , Operator "times" (Arity [Valence [] "e", Valence [] "e"] "e") "multiplication"
-    , Operator "cat" (Arity [Valence [] "e", Valence [] "e"] "e") "concatenation"
-    , Operator "len" (Arity [Valence [] "e"] "e") "length"
+    , Operator "num" (Arity [Valence [] "n"] "Exp") "numeral"
+    , Operator "str" (Arity [Valence [] "s"] "Exp") "literal"
+    , Operator "plus" (Arity [Valence [] "Exp", Valence [] "Exp"] "Exp") "addition"
+    , Operator "times" (Arity [Valence [] "Exp", Valence [] "Exp"] "Exp") "multiplication"
+    , Operator "cat" (Arity [Valence [] "Exp", Valence [] "Exp"] "Exp") "concatenation"
+    , Operator "len" (Arity [Valence [] "Exp"] "Exp") "length"
     -- TODO:
     -- * the book specifies this arity as
     --   - `let(e1;x.e2)`
     --   - `(Exp, Exp.Exp)Exp`
     -- * is it known that the `x` binds `e1`?
     -- * where is `x` specified?
-    , Operator "let" (Arity [Valence [] "e", Valence ["e"] "e"] "e") "definition"
+    , Operator "let" (Arity [Valence [] "Exp", Valence ["Exp"] "Exp"] "Exp") "definition"
     ])
   ]
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  putStrLn "syntax:\n"
+  putDoc (pretty eChart)
+  putStrLn "\n"
+  putStrLn "judgements:\n"
+  putDoc (pretty natJudgements)
+  putStrLn "\n"
