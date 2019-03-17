9533246e0c14bdf91e58931e081b137240074d23
Joel Burget <joelburget@gmail.com>
2019-03-17 15:30:04 -0700

    Clarify the monad we're using in several places.

--- src/Lvca/Bidirectional.hs
+++ src/Lvca/Bidirectional.hs
@@ -17,6 +17,7 @@ import qualified Data.Map        as Map
 import Data.Monoid               (First(First, getFirst))
 import qualified Data.Set        as Set
 import Data.Text                 (Text)
+import qualified Data.Text       as Text
 
 import Lvca.Util
 
@@ -63,35 +64,39 @@ data Env = Env
 
 makeLenses ''Env
 
-type Check a = ReaderT Env (Except ()) a
+type Check a = ReaderT Env (Except String) a
 
-runCheck :: Env -> Check a -> Either () a
+runCheck :: Env -> Check a -> Either String a
 runCheck env c = runExcept $ runReaderT c env
 
 runCheck' :: Env -> Check a -> Maybe a
 runCheck' env c = case runCheck env c of
-  Left () -> Nothing
+  Left _  -> Nothing
   Right a -> Just a
 
 data Typing = Term :< Term
 
+type MonadCheck m = (Alternative m, MonadError String m)
 type MonadMaybe m = (Alternative m, MonadError () m)
 
-checkEq :: MonadMaybe m => Term -> Term -> m ()
+checkEq :: MonadCheck m => Term -> Term -> m ()
 checkEq (Term t1 ts1) (Term t2 ts2)
   = if t1 == t2
-    then join $ sequence_ <$> fromMaybe (pairWith checkEq' ts1 ts2)
-    else empty
-checkEq (Var a) (Var b) = if a == b then pure () else empty
+    then join $ sequence_ <$> fromMaybe "checkEq mismatched term lengths"
+           (pairWith checkEq' ts1 ts2)
+    else throwError $ "checkEq mismatched terms: " ++ Text.unpack t1 ++
+           " vs " ++ Text.unpack t2
+checkEq (Var a) (Var b) = if a == b then pure () else throwError $
+  "unequal vars: " ++ Text.unpack a ++ " vs " ++ Text.unpack b
 checkEq Var{} Term{} = pure ()
 checkEq Term{} Var{} = pure ()
 
 -- XXX renaming
-checkEq' :: MonadMaybe m => ([Text], Term) -> ([Text], Term) -> m ()
+checkEq' :: MonadCheck m => ([Text], Term) -> ([Text], Term) -> m ()
 checkEq' (_, t1) (_, t2) = checkEq t1 t2
 
 -- | Match a pattern (on the left) with a term (containing no variables).
-matchPatternVars :: MonadMaybe m => Term -> Term -> m (Map Text Term)
+matchPatternVars :: Term -> Term -> Maybe (Map Text Term)
 matchPatternVars (Var v) tm
   = pure $ Map.singleton v tm
 matchPatternVars (Term head1 args1) (Term head2 args2)
@@ -99,91 +104,99 @@ matchPatternVars (Term head1 args1) (Term head2 args2)
   && length args1 == length args2
   = Map.unions <$> traverse (uncurry matchPatternVars') (zip args1 args2)
 matchPatternVars _ _
-  = empty
+  = Nothing
 
 -- XXX renaming
-matchPatternVars'
-  :: MonadMaybe m
-  => ([Text], Term) -> ([Text], Term) -> m (Map Text Term)
+matchPatternVars' :: ([Text], Term) -> ([Text], Term) -> Maybe (Map Text Term)
 matchPatternVars' (_, t1) (_, t2) = matchPatternVars t1 t2
 
-instantiate :: MonadMaybe m => Map Text Term -> Term -> m Term
+instantiate :: MonadCheck m => Map Text Term -> Term -> m Term
 instantiate env (Term tag subtms)
   = Term tag <$> traverse (instantiate' env) subtms
 instantiate env (Var v)
-  = env ^? ix v ?? ()
+  = env ^? ix v ?? ("instantiate couldn't find variable " ++ Text.unpack v)
 
-instantiate' :: MonadMaybe m => Map Text Term -> ([Text], Term) -> m ([Text], Term)
+instantiate' :: MonadCheck m => Map Text Term -> ([Text], Term) -> m ([Text], Term)
 instantiate' env (names, tm)
   = (names,) <$> instantiate (Map.withoutKeys env (Set.fromList names)) tm
 
-fromMaybe :: MonadMaybe m => Maybe a -> m a
-fromMaybe m = m ?? ()
-
--- TODO: use Maybe here instead of Check
+fromMaybe :: MonadCheck m => String -> Maybe a -> m a
+fromMaybe msg m = m ?? msg
 
 check :: Typing -> Check ()
-check (tm :< ty) = join $ ReaderT $ \Env{_rules} -> fromMaybe $ getFirst $
-  foldMap
-    (First . \case
-      Rule _ InferenceRule{} -> empty
-      Rule hyps (CheckingRule (ruleTm :<= ruleTy)) -> do
-        tmAssignments <- matchPatternVars ruleTm tm
-        tyAssignments <- matchPatternVars ruleTy ty
+check (tm :< ty) = do
+  Env{_rules} <- ask
+  let matchedRule :: Maybe (Check ())
+      matchedRule = getFirst $ foldMap
+        (First . \case
+          Rule _ InferenceRule{} -> Nothing
+          Rule hyps (CheckingRule (ruleTm :<= ruleTy)) -> do
+            tmAssignments <- matchPatternVars ruleTm tm
+            tyAssignments <- matchPatternVars ruleTy ty
 
-        pure $ flip evalStateT Map.empty $
-          for_ hyps $ \case
-            (patternCtx, CheckingRule  (hypTm :<= hypTy)) -> do
-              ctxState <- get
-              let ctx = ctxState <> patternCtx
-              tm'  <- instantiate tmAssignments hypTm
-              ty'  <- instantiate tyAssignments hypTy
-              ctx' <- traverse (instantiate tyAssignments) ctx
-              lift $ local (varTypes <>~ ctx') $ check $ tm' :< ty'
-            (patternCtx, InferenceRule (hypTm :=> hypTy)) -> do
-              ctxState <- get
-              let ctx = ctxState <> patternCtx
-              ctx' <- traverse (instantiate tyAssignments) ctx
+            Just $ flip evalStateT Map.empty $
+              for_ hyps $ \case
+                (patternCtx, CheckingRule (hypTm :<= hypTy)) -> do
+                  ctxState <- get
+                  let ctx = ctxState <> patternCtx
+                  tm'  <- instantiate tmAssignments hypTm
+                  ty'  <- instantiate tyAssignments hypTy
+                  ctx' <- traverse (instantiate tyAssignments) ctx
+                  lift $ local (varTypes <>~ ctx') $ check $ tm' :< ty'
+                (patternCtx, InferenceRule (hypTm :=> hypTy)) -> do
+                  ctxState <- get
+                  let ctx = ctxState <> patternCtx
+                  ctx' <- traverse (instantiate tyAssignments) ctx
 
-              tm'  <- instantiate tmAssignments hypTm
-              ty'  <- instantiate tyAssignments hypTy
-              ty'' <- lift $ local (varTypes <>~ ctx') $ infer tm'
-              checkEq ty' ty'')
-    _rules
+                  tm'  <- instantiate tmAssignments hypTm
+                  ty'  <- instantiate tyAssignments hypTy
+                  ty'' <- lift $ local (varTypes <>~ ctx') $ infer tm'
+                  checkEq ty' ty'')
+        _rules
+  case matchedRule of
+    Just rule -> rule
+    Nothing   -> throwError $ "No matching checking rule found for " ++ show tm
 
 infer :: Term -> Check Term
-infer tm = (join $ ReaderT $ \Env{_rules} -> fromMaybe $ getFirst $
-  foldMap
-    (First . \case
-      Rule _ CheckingRule{} -> empty
-      Rule hyps (InferenceRule (ruleTm :=> ruleTy)) -> do
-        assignments <- matchPatternVars ruleTm tm
+infer tm = do
+  Env {_rules} <- ask
+  let matchedRule = getFirst $ foldMap
+        (First . \case
+          Rule _ CheckingRule{} -> Nothing
+          Rule hyps (InferenceRule (ruleTm :=> ruleTy)) -> do
+            assignments <- matchPatternVars ruleTm tm
 
-        pure $ flip evalStateT Map.empty $ do
-          for_ hyps $ \case
-            (patternCtx, CheckingRule  (hypTm :<= hypTy)) -> do
+            pure $ flip evalStateT Map.empty $ do
+              for_ hyps $ \case
+                (patternCtx, CheckingRule  (hypTm :<= hypTy)) -> do
+                  ctxState <- get
+                  let ctx = ctxState <> patternCtx
+                  tm' <- instantiate (assignments <> ctx) hypTm
+                  ty' <- instantiate (assignments <> ctx) hypTy
+                  lift $ local (varTypes <>~ ctx) $ check $ tm' :< ty'
+                (patternCtx, InferenceRule (hypTm :=> hypTy)) -> do
+                  ctxState <- get
+                  let ctx = ctxState <> patternCtx
+                  tm' <- instantiate assignments hypTm
+                  ty' <- lift $ local (varTypes <>~ ctx) $ infer tm'
+
+                  -- Update the binding for any type variable we've infered
+                  learnedTys <- matchPatternVars hypTy ty' ?? "TODO: msg"
+                  id <>= learnedTys
+
+                  checkEq hypTy ty'
+
               ctxState <- get
-              let ctx = ctxState <> patternCtx
-              tm' <- instantiate (assignments <> ctx) hypTm
-              ty' <- instantiate (assignments <> ctx) hypTy
-              lift $ local (varTypes <>~ ctx) $ check $ tm' :< ty'
-            (patternCtx, InferenceRule (hypTm :=> hypTy)) -> do
-              ctxState <- get
-              let ctx = ctxState <> patternCtx
-              tm' <- instantiate assignments hypTm
-              ty' <- lift $ local (varTypes <>~ ctx) $ infer tm'
+              instantiate (assignments <> ctxState) ruleTy)
+        _rules
 
-              -- Update the binding for any type variable we've infered
-              learnedTys <- matchPatternVars hypTy ty'
-              id <>= learnedTys
-
-              checkEq hypTy ty'
-
-          ctxState <- get
-          instantiate (assignments <> ctxState) ruleTy)
-    _rules) <|> ctxInfer tm
+  case matchedRule of
+    Just rule -> rule
+    Nothing   -> ctxInfer tm
 
 ctxInfer :: Term -> Check Term
 ctxInfer = \case
-  Term{} -> empty
-  Var v  -> view (varTypes . at v) >>= fromMaybe
+  Var v -> view (varTypes . at v) >>=
+    fromMaybe ("Variable not found in context: " ++ Text.unpack v)
+  tm    -> throwError $
+    "found no matching inference rule for this term: " ++ show tm
